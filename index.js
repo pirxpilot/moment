29508b6a1418f27c670968f2dba09cd4f77288ea
Damian Krzeminski <pirxpilot@code42day.com>
2014-05-20 08:57:46 -0400

    improve extend - fastest and optimizeable version

--- index.js
+++ index.js
@@ -262,10 +262,10 @@
 
 
     function extend(a, b) {
-        for (var i in b) {
-            if (b.hasOwnProperty(i)) {
-                a[i] = b[i];
-            }
+        var i, prop, keys = Object.keys(b), l = keys.length;
+        for(i = 0; i < l; i++) {
+            prop = keys[i];
+            a[prop] = b[prop];
         }
         return a;
     }
